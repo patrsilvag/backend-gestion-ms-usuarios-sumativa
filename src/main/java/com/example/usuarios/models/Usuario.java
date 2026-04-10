99b278de8abd5c99615800ae5e34669ee612628a
patrsilvag <patr.silvag@duocuc.cl>
2026-04-10 17:53:36 -0400

    security:  @CrossOrigin para autorizar a Angular en controller

--- src/main/java/com/example/usuarios/models/Usuario.java
+++ src/main/java/com/example/usuarios/models/Usuario.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
